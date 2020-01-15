e628f2196f273dc7aa253843e6edbb886ba2d7cc
GinaMeini <55956463+GinaMeini@users.noreply.github.com>
2020-01-15 09:36:27 +0100

    added pictures, changed from ts to js again

--- wizard.js
+++ wizard.js
@@ -2,25 +2,39 @@ $(document).ready(function() {
     var spielbrett = document.getElementById('Spieltisch');
     spielfeld = spielbrett.getContext('2d');
     console.log(spielfeld);
-    public class Deck {
-        
-        /**
-         * array of 52 or 54 cards
-         */
-        private Card[] deck;
-        
-        /** 
-         * Keeps track of the number of  cards that have been dealt from deck
-         */ 
-        private int cardsUsed;
-        
-        /**
-         * constructs the deck
-         * initially the cards are sorted
-         * the shuffle() method can be called to randomize the order
-         */
-        public Deck() {
-            this(false); //Just call the other constructor in this class
+    let cardDeck =[];
+    let Trump;
+    let cardBack = 'gray_back'
+    let cardStrock = [];
+    let discardPile = [];
+
+    let foolRankMask = [[N,N,N,N],[1,1,1,1],[2,2,2,2],[3,3,3,3],[4,4,4,4],[5,5,5,5],
+    [6,6,6,6],[7,7,7,7],[8,8,8,8],[9,9,9,9],[10,10,10,10],[11,11,11,11],
+    [12,12,12,12],[13,13,13,13],[W,W,W,W]];
+
+    function renderStock(cardsDeck, divId, showTrump) {
+        let div = document.getElementById(divId);
+        div.innerHTML = '';
+        div.replaceWith(div);
+        let i = 0;
+        if (showTrump) {
+            let lastCard = document.createElement('Images');
+            lastCard.src = '/Images/cards/' + cardDeck[0].name + '.jpg';
+            lastCard.id = cardDeck[0].name;
+            lastCard.className = 'Trump';
+            div.append(lastCard);
+            i = 1;
         }
-    }
-});
+        for (; i < cardDeck.length; i++) {
+            let top = (-20 - i * 0.25) + 'px';
+            let left = (40 + i * 0.25) + 'px';
+            let cardId = 'card' + i;
+            let lastCard = document.createElement('Images');
+            lastCard.src = '/Images/backs/' + cardBack + '.jpg';
+            lastCard.id = cardId;
+            lastCard.className = 'cardInPack';
+            lastCard.style = 'top: ' + top + '; left: ' + left + ';';
+            div.append(lastCard);
+        }
+    }        
+})
